2794e3cfbab231261fd39f1c935ca826d1d28190
Maksim T <verdo.ya.ry19@gmail.com>
2023-08-27 19:40:59 +0400

    edit test

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/m9nlptgkq8w6056q?svg=true)](https://ci.appveyor.com/project/Maksim-Tukmachev/selenide)
--- src/test/java/CartDeliveryTest.java
+++ src/test/java/CartDeliveryTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -18,11 +19,11 @@ public class CartDeliveryTest {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Казань");
         String currentDate = generateDate(4, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").
-        $("[data-test-id='date'] input").
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
         $("[data-test-id='name'] input").setValue("Калыкин-Калык-Максимович");
         $("[data-test-id='phone'] input").setValue("+78005556767");
-        $("[data-test-id='agreement'] input").click();
+        $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
